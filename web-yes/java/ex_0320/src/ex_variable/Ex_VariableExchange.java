68d092bab9f25de020815131b938775278822141
euisang0409-prog <euisang0409@gmail.com>
2026-03-23 09:55:51 +0900

    Add files via upload

---
+++ web-yes/java/ex_0320/src/ex_variable/Ex_VariableExchange.java
@@ -0,0 +1,22 @@
+package ex_variable;
+
+public class Ex_VariableExchange {
+
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 5;
+		// 변수 x에 들어있는 값과 변수 y에 들어있는 값을
+		// 교환해주세요
+		// 필요하다면 변수를 또 만들어도 된다.
+		
+		int b = x;
+		System.out.println("b: " + x);
+		int v = y;
+		System.out.println("v: " + y );
+		
+		
+		
+		
+	}
+
+}
